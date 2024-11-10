a5042b752d531f4d43b54449336b3c155728877d
temurcyber <temurcyber@gmail.com>
2024-11-10 20:52:15 +0500

    oddiy kod

---
+++ temur.py
@@ -0,0 +1,7 @@
+ism=input("Ismingizni kiriting\n>>>")
+if ism=="temur":
+    print("Siz zo`rsiz")
+elif ism=="temur2":
+    print("Siz oddiy odamsiz")
+elif ism=="temur3":
+    print("sizni tanimayman")
